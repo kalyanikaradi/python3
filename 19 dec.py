676cf8028f23d3bd9dfe3f87f974c2a4f0aee9b8
kalyanikaradi <154000642+kalyanikaradi@users.noreply.github.com>
2023-12-19 13:43:00 +0530

    Add files via upload

---
+++ 19 dec.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#addition of two numbers using function
+def addition():
+    a=8
+    b=6
+    c=a+b
+    print(c)
+
+
+# In[2]:
+
+
+addition()
+
+
+# In[4]:
+
+
+#substraction
+def substraction():
+    a=90
+    b=65
+    c=a-b
+    print(c)
+
+
+# In[5]:
+
+
+substraction()
+
+
+# In[6]:
+
+
+#multiplication
+def multiplication():
+    a=79
+    b=34
+    c=a*b
+    print(c)
+
+
+# In[7]:
+
+
+multiplication()
+
+
+# In[8]:
+
+
+#division()
+def division():
+    a=78
+    b=5
+    c=a/b
+    print(c)
+
+
+# In[10]:
+
+
+division()
+
+
+# In[11]:
+
+
+#area of triangle
+def area_triangle(b,h):
+    area=b*h
+    print(area)
+    
+
+
+# In[13]:
+
+
+area_triangle(6,7)
+
+
+# In[17]:
+
+
+#area of square
+def area_square(side):
+    area=4*side
+    print(area)
+
+
+# In[18]:
+
+
+area_square(7)
+
+
+# In[19]:
+
+
+#area of reactangle
+def area_reactangle(l,w):
+    area=l*w
+    print(area)
+
+
+# In[20]:
+
+
+area_reactangle(7,3)
+
+
+# In[21]:
+
+
+#area of circle
+def area_circle(radius):
+    area=3.14*radius
+    print(area)
+
+
+# In[22]:
+
+
+area_circle(70)
+
+
+# In[23]:
+
+
+#area of cube
+def area_cube(a):
+    area=6*a*a
+    print(area)
+
+
+# In[24]:
+
+
+area_cube(6)
+
+
+# In[ ]:
+
+
+
+
